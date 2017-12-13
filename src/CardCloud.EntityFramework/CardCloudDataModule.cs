9f07f95e77806cc4d7716a4d91ce3a09e4eb3e88
yuanhuawei <yuanhuawei@alilyun.com>
2017-12-13 17:16:59 +0800

    First Commit

---
+++ src/CardCloud.EntityFramework/CardCloudDataModule.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+using System.Reflection;
+using Abp.Modules;
+using Abp.Zero.EntityFramework;
+using CardCloud.EntityFramework;
+
+namespace CardCloud
+{
+    [DependsOn(typeof(AbpZeroEntityFrameworkModule), typeof(CardCloudCoreModule))]
+    public class CardCloudDataModule : AbpModule
+    {
+        public override void PreInitialize()
+        {
+            Database.SetInitializer(new CreateDatabaseIfNotExists<CardCloudDbContext>());
+
+            Configuration.DefaultNameOrConnectionString = "Default";
+        }
+
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ src/CardCloud.EntityFramework/Migrations/AbpZeroDbMigrator.cs
@@ -0,0 +1,22 @@
+﻿using Abp.Dependency;
+using Abp.Domain.Uow;
+using Abp.MultiTenancy;
+using Abp.Zero.EntityFramework;
+using CardCloud.EntityFramework;
+
+namespace CardCloud.Migrations
+{
+    public class AbpZeroDbMigrator : AbpZeroDbMigrator<CardCloudDbContext, Configuration>
+    {
+        public AbpZeroDbMigrator(
+            IUnitOfWorkManager unitOfWorkManager,
+            IDbPerTenantConnectionStringResolver connectionStringResolver,
+            IIocResolver iocResolver)
+            : base(
+                unitOfWorkManager,
+                connectionStringResolver,
+                iocResolver)
+        {
+        }
+    }
+}
